d88414594a4bb315d2b43b373a74875e5d9f3b47
takata-kento <takata_kento@ims-sol.co.jp>
2025-01-07 22:52:35 +0900

    主張(ASSERTION)のサンプルコードを追加

---
+++ sample/03_create/04_createother/assertion.sql
@@ -0,0 +1,26 @@
+-- 1.ASSERTIONで複数のテーブルにまたがった制約を作成することができる
+--   PostgreSQLはASSERTIONを実装していないので使用できない
+CREATE TABLE table1 (
+	countrycode character(3) PRIMARY KEY,
+	language text,
+	isofficial boolean NOT NULL,
+	percentage real NOT NULL
+);
+
+CREATE TABLE table2 (
+	id integer PRIMARY KEY,
+	name text NOT NULL,
+	countrycode character(3) NOT NULL,
+	district text NOT NULL,
+	population integer NOT NULL
+);
+
+CREATE ASSERTION population_check
+CHECK(NOT EXISTS(
+	SELECT *
+	FROM table1 t1, table2 t2
+	WHERE t1.countrycode = t2.countrycode
+	AND t2.population > 1000
+	)
+);
+
